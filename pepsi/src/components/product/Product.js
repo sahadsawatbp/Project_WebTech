f1a7bdc06fef631ade34886b2a15498f305efe9d
sahadsawatbp <80098729+sahadsawatbp@users.noreply.github.com>
2022-10-31 20:53:49 +0700

    Update Product.js

--- pepsi/src/components/product/Product.js
+++ pepsi/src/components/product/Product.js
@@ -1,11 +1,9 @@
-
 import "./Product.css"
 const Product = ({name,img_product,background_color,id,page}) =>{
     const design = background_color
     const delay = id/20
     const delay_p = id/12
     return (
-        
             <div className="product-item">
                 <p id="tooltip" ><strong>{name}</strong></p>
                 <div className="background-color" style={{background:design}}></div>
